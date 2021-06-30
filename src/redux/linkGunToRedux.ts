4bfe3fdb94694b72034b00507cf4c15f5e3209fe
Manwe <manuel.etchegaray7@gmail.com>
2021-06-30 13:39:38 -0300

    debounce gun to redux

--- src/redux/linkGunToRedux.ts
+++ src/redux/linkGunToRedux.ts
@@ -11,71 +11,87 @@ export default function linkGunToRedux() {
     // Create listeners for Gun's Graph api
     // These will check against the internal Graph data for new data and push directly to redux
     // This is done so we minimioze queries to the graph, so its Superfast!
+    let decksDebouncer: NodeJS.Timeout | null = null;
     userRef.get("decks").on(
-      async (data: any) => {
-        const object = await getGraphObjectFromPartial<Record<string, GunDeck>>(
-          data
-        );
-        // console.log("on decks", data, object);
+      (data: any) => {
+        if (decksDebouncer) clearTimeout(decksDebouncer);
+        decksDebouncer = setTimeout(async () => {
+          const object = await getGraphObjectFromPartial<
+            Record<string, GunDeck>
+          >(data);
 
-        if (object) {
-          reduxAction(dispatch, {
-            type: "SET_DECKS",
-            arg: object,
-          });
-        }
+          if (object) {
+            console.log("SET_DECKS", Object.keys(object));
+            reduxAction(dispatch, {
+              type: "SET_DECKS",
+              arg: object,
+            });
+          }
+        }, 1000);
       },
       {
         change: true,
       }
     );
 
+    let matchesDebouncer: NodeJS.Timeout | null = null;
     userRef.get("matches").on(
-      async (data: any) => {
-        const object = await getGraphObjectFromPartial<
-          Record<string, GunMatch>
-        >(data);
-        // console.log("on matches", data, object);
+      (data: any) => {
+        if (matchesDebouncer) clearTimeout(matchesDebouncer);
+        matchesDebouncer = setTimeout(async () => {
+          const object = await getGraphObjectFromPartial<
+            Record<string, GunMatch>
+          >(data);
 
-        if (object) {
-          reduxAction(dispatch, {
-            type: "SET_MATCHES",
-            arg: object,
-          });
-        }
+          if (object) {
+            console.log("SET_MATCHES", Object.keys(object));
+            reduxAction(dispatch, {
+              type: "SET_MATCHES",
+              arg: object,
+            });
+          }
+        }, 1000);
       },
       {
         change: true,
       }
     );
 
+    let uuiddataDebouncer: NodeJS.Timeout | null = null;
     userRef.get("uuidData").on(
-      async (data: any) => {
-        const object = await getGraphObjectFromPartial<GunUser["uuidData"]>(
-          data
-        );
-        console.log("on uuid data", data, object);
+      (data: any) => {
+        if (uuiddataDebouncer) clearTimeout(uuiddataDebouncer);
+        uuiddataDebouncer = setTimeout(async () => {
+          const object = await getGraphObjectFromPartial<GunUser["uuidData"]>(
+            data
+          );
 
-        if (object) {
-          reduxAction(dispatch, {
-            type: "SET_ALL_UUID_DATA",
-            arg: object,
-          });
-        }
+          if (object) {
+            console.log("SET_ALL_UUID_DATA", Object.keys(object));
+            reduxAction(dispatch, {
+              type: "SET_ALL_UUID_DATA",
+              arg: object,
+            });
+          }
+        }, 1000);
       },
       {
         change: true,
       }
     );
 
+    let decksIndexDebouncer: NodeJS.Timeout | null = null;
     userRef.get("decksIndex").on((d: any) => {
       // eslint-disable-next-line no-param-reassign
-      const data = { ...d };
-      delete data._;
-      reduxAction(dispatch, {
-        type: "SET_DECKS_INDEX",
-        arg: data,
-      });
+      if (decksIndexDebouncer) clearTimeout(decksIndexDebouncer);
+      decksIndexDebouncer = setTimeout(async () => {
+        const data = { ...d };
+        delete data._;
+        reduxAction(dispatch, {
+          type: "SET_DECKS_INDEX",
+          arg: data,
+        });
+      }, 1000);
     });
   }
 }
