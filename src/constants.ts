bb132eced3dd52c04099095b9535d7758ec4c604
Manwe <manuel.etchegaray7@gmail.com>
2021-10-19 11:27:41 -0300

    fix bump

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 // These have proven to be unreliable as ***** (got them from google *shrug*)
+
 export const DEFAULT_SERVERS = [
   // "https://stark-badlands-20144.herokuapp.com/gun",
   // "http://dev-gun.armsbook.com:8765/gun",
