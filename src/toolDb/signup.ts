c98cea471c8c15862a6aac5a8454a2c69fafba92
Manwe <manuel.etchegaray7@gmail.com>
2021-11-04 18:03:58 -0300

    remove signup console logs

--- src/toolDb/signup.ts
+++ src/toolDb/signup.ts
@@ -1,16 +1,18 @@
 import { DbUserids } from "../types/dbTypes";
 import login from "./login";
 
+/**
+ * Sign up process
+ * @param username plaintext username
+ * @param password sha1 of the plaintext password
+ * @returns Promise<PutMessage>
+ */
 export default function signup(username: string, password: string) {
-  console.log("signup", username, password);
   return window.toolDb.signUp(username, password).then((msg) => {
-    console.log("msg", msg);
-    return login(username, password).then((keys) => {
-      console.log("keys", keys);
+    return login(username, password).then((_keys) => {
       return window.toolDb
         .putData<DbUserids>("userids", {}, true)
-        .then((put) => {
-          console.log("put", put);
+        .then((_put) => {
           return msg;
         });
     });
