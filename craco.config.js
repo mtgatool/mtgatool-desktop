53a8d5d19fed61827861d28bd2b0d2f7539ecdd0
Manwe <manuel.etchegaray7@gmail.com>
2022-02-11 17:31:16 -0300

    test sourcemaps and no minification

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,14 @@
 const ModuleReplacement = require("./module-resolver-file");
 const eslintConfig = require("./.eslintrc");
 
+process.env.GENERATE_SOURCEMAP = true;
+
 // https://www.npmjs.com/package/@craco/craco
 module.exports = {
   webpack: {
+    optimization: {
+      minimize: false,
+    },
     configure: {
       target: "electron-renderer",
       module: {
