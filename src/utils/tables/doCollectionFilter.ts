f89efe7ad69f11c41624f7b86a372e68a28096a8
Manwe <manuel.etchegaray7@gmail.com>
2022-07-30 16:54:47 -0300

    remove debugs

--- src/utils/tables/doCollectionFilter.ts
+++ src/utils/tables/doCollectionFilter.ts
@@ -16,7 +16,6 @@ export default function doCollectionFilter(
   filters: Filters<CardsData>,
   sort: Sort<CardsData> = { key: "", sort: 1 }
 ): CardsData[] {
-  console.log("doCollectionFilter", filters);
   let filteredData = data;
 
   filters.forEach((element) => {
