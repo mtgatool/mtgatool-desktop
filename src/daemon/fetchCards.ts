2bbdb03f9cb47b256a180e322adc21d46ce6634a
Manwë <manuel.etchegaray7@gmail.com>
2024-03-21 08:11:28 -0300

    try catch cards read

--- src/daemon/fetchCards.ts
+++ src/daemon/fetchCards.ts
@@ -6,7 +6,13 @@ export default function fetchCards() {
   fetchPlayerId();
   const reader = globalData.mtgaReader;
 
-  const { cards } = reader.read("mtgaInventory") as any;
+  let cards = null;
+  try {
+    const inventory = reader.read("mtgaInventory") as any;
+    cards = inventory?.cards;
+  } catch (e) {
+    console.debug("Error fetching cards", e);
+  }
   console.debug("cards info", cards);
 
   if (cards?.data) {
