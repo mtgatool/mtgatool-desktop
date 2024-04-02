41edd67280d8455baddd8db7d3ff77caa3aa3b6f
Manwë <manuel.etchegaray7@gmail.com>
2024-04-02 18:13:37 -0300

    Update getPlayerNameWithoutSuffix.ts

--- src/utils/getPlayerNameWithoutSuffix.ts
+++ src/utils/getPlayerNameWithoutSuffix.ts
@@ -1,7 +1,8 @@
-const suffixLength = "#12345".length;
-
 export default function getPlayerNameWithoutSuffix(
   playerName: string | undefined
 ): string {
-  return playerName ? playerName.slice(0, -suffixLength) : "-";
+  const hashPos = playerName?.indexOf("#") || -1;
+  return playerName
+    ? playerName.slice(0, hashPos > -1 ? hashPos : playerName.length)
+    : "-";
 }
