833ef92fb8e1b84a1f81dd116dfc20defb59b860
Manwë <manuel.etchegaray7@gmail.com>
2024-05-14 09:40:29 -0300

    Update RankIcon.tsx

--- src/components/RankIcon.tsx
+++ src/components/RankIcon.tsx
@@ -11,8 +11,12 @@ export interface RankIconProps {
 }
 
 export default function RankIcon(props: RankIconProps): JSX.Element {
-  const { rank, tier, style, step, format, percentile, leaderboardPlace } =
-    props;
+  const { tier, style, step, format, percentile, leaderboardPlace } = props;
+
+  let { rank } = props;
+
+  if (percentile !== 0 && !rank) rank = "Mythic";
+
   const rankIndex = getRankIndex(rank, tier);
 
   const newStyle = { ...{ width: "48px", height: "48px" }, ...style };
